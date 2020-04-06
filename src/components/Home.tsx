36e68a78fd9bcd1abbc88857001527860f214c5c
Bryan Grill <grillb@gmail.com>
2020-04-05 21:37:45 -0400

    breakout stats component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,9 @@
 import { h, FunctionalComponent } from '/web_modules/preact.js';
 import { useState, useEffect } from '/web_modules/preact/hooks.js';
-import moment from '/web_modules/moment.js';
+
 import { html } from '/web_modules/htm/preact.js';
+
+import Stats from './Stats.js';
 
 const resources = [
   {
@@ -57,18 +59,6 @@ const initialState = {
 const Home: FunctionalComponent = () => {
   const [usData, setData] = useState(initialState);
 
-  const sinceUpdate = (updateTime: string | null) => {
-    if (updateTime) {
-      // @ts-ignore
-      const now = new moment();
-      const then = moment(updateTime);
-      const since = moment.duration(then.diff(now)).humanize(true);
-      return since;
-    }
-
-    return null;
-  };
-
   const fetchUS = () => {
     fetch('https://covidtracking.com/api/us')
       .then((response) => {
@@ -86,45 +76,29 @@ const Home: FunctionalComponent = () => {
     fetchUS();
   }, []);
 
-  return html`<div>
-    <div class="pb-3">
-      <h3>Latest US Numbers</h3>
-      <div class="d-lg-flex justify-content-lg-center">
-        <ul class="list-group list-group-horizontal-sm">
-          <li class="list-group-item list-group-item-danger">
-            Positive: ${usData.data.positive}
-          </li>
-          <li class="list-group-item list-group-item-success">Recovered: ${
-            usData.data.recovered
-          }</li>
-          <li class="list-group-item list-group-item-secondary">Fatal: ${
-            usData.data.death
-          }</li>
-        </ul>
+  return (
+    <div>
+      <Stats data={usData.data} />
+      <div class="pb-3">
+        <h3>
+          Resources <i class="fas fa-external-link-square-alt"></i>{' '}
+        </h3>
+        <div class="list-group">
+          {resources.map((i) => {
+            return (
+              <a
+                class="list-group-item list-group-item-action"
+                href={i.link(window.innerWidth <= 800)}
+                target="_blank"
+              >
+                {i.title}
+              </a>
+            );
+          })}
+        </div>
       </div>
-      <div class="py-2">
-        <p class="text-lg-center font-weight-light" style="font-size: 10px;">Via <a href="https://covidtracking.com" target="_blank">The COVID Tracking Project</a>, last updated: ${sinceUpdate(
-          usData.data.lastModified,
-        )}</p>
-      </div>
     </div>
-    <div class="pb-3">
-    <h3>Resources <i class="fas fa-external-link-square-alt"></i> </h1>
-    <div class="list-group">
-      ${resources.map((i) => {
-        return (
-          <a
-            class="list-group-item list-group-item-action"
-            href={i.link(window.innerWidth <= 800)}
-            target="_blank"
-          >
-            {i.title}
-          </a>
-        );
-      })}
-    </div>
-    </div>
-  </div>`;
+  );
 };
 
 export default Home;
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,15 +1,53 @@
-import { h, FunctionalComponent } from '/web_modules/preact.js';
+import { h } from '/web_modules/preact.js';
 import { html } from '/web_modules/htm/preact.js';
+import moment from '/web_modules/moment.js';
 
-const Stats: FunctionalComponent = ({
-  stats,
+const sinceUpdate = (updateTime: string | null) => {
+  if (updateTime) {
+    // @ts-ignore
+    const now = new moment();
+    const then = moment(updateTime);
+    const since = moment.duration(then.diff(now)).humanize(true);
+    return since;
+  }
+
+  return null;
+};
+
+const Stats = ({
+  data,
 }: {
-  positive: string;
-  recovered: string;
-  death: string;
-  lastModified: string | null;
+  data: {
+    positive: string;
+    recovered: string;
+    death: string;
+    lastModified: string | null;
+  };
 }) => {
-  return <div></div>;
+  return html` <div class="pb-3">
+    <h3>Latest US Numbers</h3>
+    <div class="d-lg-flex justify-content-lg-center">
+      <ul class="list-group list-group-horizontal-sm">
+        <li class="list-group-item list-group-item-danger">
+          Positive: ${data.positive}
+        </li>
+        <li class="list-group-item list-group-item-success">
+          Recovered: ${data.recovered}
+        </li>
+        <li class="list-group-item list-group-item-secondary">
+          Fatal: ${data.death}
+        </li>
+      </ul>
+    </div>
+    <div class="py-2">
+      <p class="text-lg-center font-weight-light" style="font-size: 10px;">
+        Via
+        <a href="https://covidtracking.com" target="_blank"
+          >The COVID Tracking Project</a
+        >, updated ${sinceUpdate(data.lastModified)}
+      </p>
+    </div>
+  </div>`;
 };
 
 export default Stats;
